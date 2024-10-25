fd69fd207dfb6f677f64b2baf0f932ec84b8806f
maxisam <456807+maxisam@users.noreply.github.com.>
2024-10-24 23:04:10 -0500

    Refactor: move registerCommands to core/commands

--- src/core/commands.ts
+++ src/core/commands.ts
@@ -1,4 +1,10 @@
+import * as vscode from 'vscode';
+
+import type { DirectoryProvider } from '../provider/DirectoryProvider';
+import type { IConfiguration } from '../types/Configuration';
+import type { FileSystemObject } from '../types/FileSystemObject';
 import { EXTENSION_NAME } from './constants';
+import { clearScope, scopeToThis } from './scope';
 
 export enum vsCodeCommands {
     Open = 'vscode.open',
@@ -14,4 +20,95 @@ export enum ExtensionCommands {
     RenameItem = `${EXTENSION_NAME}.renameItem`,
     FocusInExplorer = `${EXTENSION_NAME}.focusInExplorer`,
     ClearFocusScope = `${EXTENSION_NAME}.clearFocusScope`,
+}
+
+export function registerCommands(
+    context: vscode.ExtensionContext,
+    directoryProvider: DirectoryProvider,
+    config: IConfiguration,
+    configDirUri: vscode.Uri,
+) {
+    context.subscriptions.push(
+        registerRefreshEntryCommand(directoryProvider),
+        registerOpenItemCommand(),
+        registerSelectItemCommand(directoryProvider),
+        registerRemoveItemCommand(directoryProvider),
+        registerRenameItemCommand(directoryProvider),
+        registerCantRemoveItemCommand(),
+        registerRemoveAllItemsCommand(directoryProvider),
+        registerFocusInExplorerCommand(config, configDirUri),
+        registerClearFocusScopeCommand(config, configDirUri),
+    );
+}
+
+function registerRefreshEntryCommand(directoryProvider: DirectoryProvider) {
+    return vscode.commands.registerCommand(ExtensionCommands.RefreshEntry, () =>
+        directoryProvider.refresh(),
+    );
+}
+
+function registerOpenItemCommand() {
+    return vscode.commands.registerCommand(ExtensionCommands.OpenItem, (file) => {
+        vscode.commands.executeCommand(
+            vsCodeCommands.Open,
+            vscode.Uri.parse(file.resourceUri.path),
+        );
+    });
+}
+
+function registerSelectItemCommand(directoryProvider: DirectoryProvider) {
+    return vscode.commands.registerCommand(
+        ExtensionCommands.SelectItem,
+        async (args) => await directoryProvider.selectItemAsync(vscode.Uri.parse(args.path)),
+    );
+}
+
+function registerRemoveItemCommand(directoryProvider: DirectoryProvider) {
+    return vscode.commands.registerCommand(ExtensionCommands.RemoveItem, async (args) => {
+        await directoryProvider.removeItemAsync(args.resourceUri);
+    });
+}
+
+function registerRenameItemCommand(directoryProvider: DirectoryProvider) {
+    return vscode.commands.registerCommand(ExtensionCommands.RenameItem, async (args) => {
+        await directoryProvider.renameItemAsync(args);
+    });
+}
+
+function registerCantRemoveItemCommand() {
+    return vscode.commands.registerCommand(ExtensionCommands.CantRemoveItem, () => {
+        vscode.window.showInformationMessage(
+            'You can only remove items that were directly added to the view',
+        );
+    });
+}
+
+function registerRemoveAllItemsCommand(directoryProvider: DirectoryProvider) {
+    return vscode.commands.registerCommand(
+        ExtensionCommands.RemoveAllItems,
+        async () => await directoryProvider.removeAllItemsAsync(),
+    );
+}
+
+function registerFocusInExplorerCommand(config: IConfiguration, configDirUri: vscode.Uri) {
+    return vscode.commands.registerCommand(
+        ExtensionCommands.FocusInExplorer,
+        async (element: FileSystemObject) => {
+            const path = element.resourceUri;
+            if (!path) {
+                vscode.window.showInformationMessage(
+                    'Use this command from the Explorer context menu.',
+                );
+                return;
+            }
+            await clearScope(config, configDirUri);
+            await scopeToThis(path, config, configDirUri);
+        },
+    );
+}
+
+function registerClearFocusScopeCommand(config: IConfiguration, configDirUri: vscode.Uri) {
+    return vscode.commands.registerCommand(ExtensionCommands.ClearFocusScope, async () => {
+        await clearScope(config, configDirUri);
+    });
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
 
-import { ExtensionCommands, vsCodeCommands } from './core/commands';
+import { registerCommands } from './core/commands';
 import { REGISTER_TREE_DATA_PROVIDER } from './core/constants';
-import { clearScope, initScope, scopeToThis } from './core/scope';
+import { initScope } from './core/scope';
 import { DirectoryOperator } from './operator/DirectoryOperator';
 import { DirectoryProvider } from './provider/DirectoryProvider';
-import type { FileSystemObject } from './types/FileSystemObject';
 import { getConfigurationAsync, getConfigurationDirUri } from './utils/configUtils';
 
 export async function activate(context: vscode.ExtensionContext) {
@@ -15,54 +14,7 @@ export async function activate(context: vscode.ExtensionContext) {
     const directoryOperator = new DirectoryOperator(context, config, configDirUri);
     const directoryProvider = new DirectoryProvider(directoryOperator);
     vscode.window.registerTreeDataProvider(REGISTER_TREE_DATA_PROVIDER, directoryProvider);
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(ExtensionCommands.RefreshEntry, () =>
-            directoryProvider.refresh(),
-        ),
-        vscode.commands.registerCommand(ExtensionCommands.OpenItem, (file) => {
-            vscode.commands.executeCommand(
-                vsCodeCommands.Open,
-                vscode.Uri.parse(file.resourceUri.path),
-            );
-        }),
-        vscode.commands.registerCommand(
-            ExtensionCommands.SelectItem,
-            async (args) => await directoryProvider.selectItemAsync(vscode.Uri.parse(args.path)),
-        ),
-        vscode.commands.registerCommand(ExtensionCommands.RemoveItem, async (args) => {
-            await directoryProvider.removeItemAsync(args.resourceUri);
-        }),
-        vscode.commands.registerCommand(ExtensionCommands.RenameItem, async (args) => {
-            await directoryProvider.renameItemAsync(args);
-        }),
-        vscode.commands.registerCommand(ExtensionCommands.CantRemoveItem, () => {
-            vscode.window.showInformationMessage(
-                'You can only remove items that were directly added to the view',
-            );
-        }),
-        vscode.commands.registerCommand(
-            ExtensionCommands.RemoveAllItems,
-            async () => await directoryProvider.removeAllItemsAsync(),
-        ),
-        vscode.commands.registerCommand(
-            ExtensionCommands.FocusInExplorer,
-            async (element: FileSystemObject) => {
-                const path = element.resourceUri;
-                if (!path) {
-                    vscode.window.showInformationMessage(
-                        'Use this command from the Explorer context menu.',
-                    );
-                    return;
-                }
-                await clearScope(config, configDirUri);
-                await scopeToThis(path, config, configDirUri);
-            },
-        ),
-        vscode.commands.registerCommand(ExtensionCommands.ClearFocusScope, async () => {
-            await clearScope(config, configDirUri);
-        }),
-    );
+    registerCommands(context, directoryProvider, config, configDirUri);
 }
 
 export function deactivate() {}
