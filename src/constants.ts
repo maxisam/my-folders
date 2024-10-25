87a99b36f0f0a18fd276f33bc91a03aa80f9ea74
maxisam <456807+maxisam@users.noreply.github.com.>
2024-10-24 22:02:26 -0500

    refactor: update view item commands to use consistent naming convention

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const EXTENSION_NAME = 'my-folders';
 export const REGISTER_TREE_DATA_PROVIDER = 'my-folders';
 export const configFileName: string = 'my-folders.json';
-export const MY_FOLDER_DIRECTORY_CONTEXT = 'directlyBookmarkedDirectory';
+export const MY_FOLDER_DIRECTORY_CONTEXT = 'myFoldersDirectory';
